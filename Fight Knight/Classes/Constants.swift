006f61191e50b9cdd33766560f8700f2069624e4
jessicaeh <jeh15g@acu.edu>
2019-04-29 08:15:12 -0500

    Background and ground places

---
+++ Fight Knight/Classes/Constants.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Fight Knight
+//
+//  Created by Jessica Halbert on 4/17/19.
+//  Copyright © 2019 Jessica Halbert. All rights reserved.
+//
+
+import UIKit
+
+struct ImageName {
+    static let Background = "Background"
+    static let Ground = "Ground"
+}
+
+//struct SoundFile {
+//    static let BackgroundMusic = "CheeZeeJungle.caf"
+//    static let Slice = "Slice.caf"
+//    static let Splash = "Splash.caf"
+//    static let NomNom = "NomNom.caf"
+//}
+
+struct Layer {
+    static let Background: CGFloat = 0
+    static let Knight: CGFloat = 1
+//    static let Vine: CGFloat = 1
+//    static let Prize: CGFloat = 2
+    static let Ground: CGFloat = 1
+}
--- Fight Knight/Classes/GameScene.swift
+++ Fight Knight/Classes/GameScene.swift
@@ -13,55 +13,26 @@ import AVFoundation
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var Knight: SKSpriteNode!
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        setUpScenery()
     }
     
     fileprivate func setUpScenery() {
-//        let background = SKSpriteNode(imageNamed: ImageName.Background)
-//        background.anchorPoint = CGPoint(x: 0, y: 0)
-//        background.position = CGPoint(x: 0, y: 0)
-//        background.zPosition = Layer.Background
-//        background.size = CGSize(width: size.width, height: size.height)
-//        addChild(background)
-//
-//        let water = SKSpriteNode(imageNamed: ImageName.Water)
-//        water.anchorPoint = CGPoint(x: 0, y: 0)
-//        water.position = CGPoint(x: 0, y: 0)
-//        water.zPosition = Layer.Foreground
-//        water.size = CGSize(width: size.width, height: size.height * 0.2139)
-//        addChild(water)
-//
-//        //self.view.bringSubviewToFront(label);
-//        label.zPosition = 1;
-//        label.text = "20 seconds left"
-//        label.fontSize = 20
-//        label.fontColor = SKColor.black
-//        label.position = CGPoint(x: size.width/2, y: size.height - 50)
-//        addChild(label)
+        let background = SKSpriteNode(imageNamed: ImageName.Background)
+        background.anchorPoint = CGPoint(x: 0, y: 0)
+        background.position = CGPoint(x: 0, y: 0)
+        background.zPosition = Layer.Background
+        background.size = CGSize(width: size.width, height: size.height)
+        addChild(background)
         
+        let ground = SKSpriteNode(imageNamed: ImageName.Ground)
+        ground.anchorPoint = CGPoint(x: 0, y: 0)
+        ground.position = CGPoint(x: 0, y: 0)
+        ground.zPosition = Layer.Ground
+        ground.size = CGSize(width: size.width, height: size.height * 0.13)
+        addChild(ground)
     }
     
     
